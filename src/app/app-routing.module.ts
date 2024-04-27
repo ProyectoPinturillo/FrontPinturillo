edbd666721c4622fb00688d3823be66473a067bd
SoyRoman <romancastiblancocmail.com>
2024-04-27 11:56:31 -0500

    Segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { HomeComponent } from './components/home/home.component';
+import { CuartosPageComponent } from './cuartos/cuartos-page/cuartos-page.component';
+import { CrarCuartoComponent } from './cuartos/crar-cuarto/crar-cuarto.component';
+
+const routes: Routes = [
+  {path:'', redirectTo: 'home', pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'cuartos', component: CuartosPageComponent},
+  {path:'crearCuarto', component: CrarCuartoComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PinturilloFront'`, () => {
+  it(`should have as title 'PinturilloProyecto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PinturilloFront');
+    expect(app.title).toEqual('PinturilloProyecto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PinturilloFront app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PinturilloProyecto app is running!');
   });
 });
